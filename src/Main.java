77e137ca76efe1198c498a88cbd203f92c9430c5
HuangDongPeng <536762250@qq.cmom>
2018-03-14 20:11:49 +0800

    word count

--- src/Main.java
+++ src/Main.java
@@ -5,19 +5,12 @@ public class Main {
 
 
     public static void main(String[] args)throws Exception{
-        int countChar=0;
-        int countWord=0;
-        int countLine=0;
 
         String testPath="d:/test.c";
 
         ReadChar(testPath);
         ReadLine(testPath);
-
-        System.out.println();
-        System.out.println("char count"+countChar);
-        System.out.println("word count"+countWord);
-        System.out.println("line count"+countLine);
+        ReadWord(testPath);
 
     }
 
@@ -27,21 +20,21 @@ public class Main {
         Reader reader=null;
 
         try{
-            int wordCount=0;
+            int charCount=0;
             reader  =new InputStreamReader(new FileInputStream(file));
             int tempChar;
             char character;
             while((tempChar=reader.read())!=-1){
-            //判断是不是回车
+                //判断是不是回车
                 if(!(tempChar==13||tempChar==10))
-                    wordCount++;
+                    charCount++;
 
                 character=(char)tempChar;
                 System.out.print(" "+tempChar);
 
             }
             reader.close();
-            System.out.println("word count: "+wordCount);
+            System.out.println("word count: "+charCount);
         }catch (Exception e){
             e.printStackTrace();
         }
@@ -71,8 +64,36 @@ public class Main {
         }
     }
 
-    public  static  void ReadWord(String path){
+    public  static  void ReadWord(String path) {
+        File file = new File(path);
+        Reader reader = null;
 
+        try {
+            int wordCount = 0;
+            reader = new InputStreamReader(new FileInputStream(file));
+            int tempChar;
+            char character;
+            boolean isChar=false;
+            while ((tempChar = reader.read()) != -1) {
+               if((tempChar>=65&&tempChar<=90)||(tempChar>=97&&tempChar<=122)){
+                   isChar=true;
+                   System.out.print((char)tempChar);
+               }
+               else {
+                   if(isChar)
+                   {
+                       isChar=false;
+                       wordCount++;
+                   }
+                   continue;
+               }
+
+            }
+            reader.close();
+            System.out.println("word count: " + wordCount);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
 
